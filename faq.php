9fafb4cdb85318096d152b32d55844e7ba526c38
restufauzi1 <restuf443@gmail.com>
2021-04-02 21:44:47 +0700

    astrit

---
+++ faq.php
@@ -0,0 +1,25 @@
+<p><h2>APLIKASI SPP MAHARDHIKA</h2>
+
+
+<p>APA ITU APLIKASI SPP MAHARDHIKA?</p>
+
+<p>Aplikasi yang digunakan untuk kepentingan pembayaran uang spp dan kegiatan siswa bagi sekolah sekolah baik negri maupun swasta.</p>
+
+<p>Mengapa Harus Menggunakan Aplikasi Ini?</p>
+
+<p>"Penggunaan aplikasi pembayaran sekolah dapat memudahkan sekolah dalam mengingatkan tagihan biaya pendidikan pada orang tua/wali siswa"</p>
+
+<p>- Mahardhika </p>
+
+<p>Terdapat Fitur Apa Saja Sih?</p>
+
+<p>"Aplikasi SPP Mahardhika terdapat fitur-fitur khusus sesuai level Seperti : 
+<p>1. Admin (bisa melihat,menambah,mengubah,dan mengahpus data spp,kelas,pengguna,siswa,dan transaksi pembayaran).</p>  
+<p>2. Petugas (hanya bisa melihat,menambah,mengubah,dan menghapus transaksi pembayaran saja). dan</p>
+<p>3. Siswa (hanya bisa melihat history pembayaran saja)."</p>
+
+<p>- Mahardhika</p>
+
+<p>Kalau Begitu Orang Luar Bisa Mengakses?</p>
+
+<p>"Tentu saja bisa, Cuman hanya login sebagai siswa. Jadi orang tua pun tahu apakah anaknya membayar atau tidak? dan cara mengaksesnya hanya cukup dengan mencari NISN siswa, di kolom pencaharian."</p>
